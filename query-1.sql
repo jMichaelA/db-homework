282030eea9bb6adacc965c57b77f18639b738652
kodekill <brian.falor@gmail.com>
2017-10-23 20:47:16 -0600

    Add files via upload

---
+++ query-1.sql
@@ -0,0 +1,6 @@
+SELECT DISTINCT "nameFirst", "nameLast" 
+FROM master m
+        JOIN appearances a ON m."masterID" = a."masterID"
+        JOIN teams t ON t."teamID" = a."teamID" 
+WHERE t."name" = 'Los Angeles Dodgers'
+ORDER BY "nameLast", "nameFirst" ASC
