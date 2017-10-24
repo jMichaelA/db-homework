bd4be82fd3c0af69e3c265240571c4205342e7d3
kodekill <brian.falor@gmail.com>
2017-10-23 20:32:45 -0600

    Add files via upload

---
+++ query-3.sql
@@ -0,0 +1,6 @@
+SELECT DISTINCT "nameFirst", "nameLast" 
+FROM master m
+        JOIN pitching p ON p."masterID" = m."masterID"
+        JOIN teams t ON t."teamID" = p."teamID" 
+WHERE p."teamID" = 'MON'
+ORDER BY "nameLast", "nameFirst" ASC 
