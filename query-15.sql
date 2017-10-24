589b03281dadca4e5dc2b3638b37b5b5f943d68c
Jacob <jacob.m.adams@gmail.com>
2017-10-24 14:13:43 -0600

    15: fixed query to be right rank

--- query-15.sql
+++ query-15.sql
@@ -1,19 +1,20 @@
 with ranking as (
     SELECT row_number() OVER (ORDER BY tw.wins desc) as rank, tw."teamID"
         FROM (
-              SELECT sum(t."W") as wins, t."teamID"
+              SELECT sum(t."W") as wins, t."teamID", t."lgID"
                    FROM teams t
               WHERE t."yearID" >= 1970
               AND t."yearID" <= 1979
-              GROUP BY "teamID"
+              AND t."lgID" = 'NL'
+              GROUP BY "teamID", t."lgID"
              ) tw
 )
 
-SELECT DISTINCT t.name
+SELECT DISTINCT t.name, rk.rank -1 as rank
     FROM ranking rk
     JOIN 
     (
-        SELECT count(*)/2 as half FROM ranking
+        SELECT CASE WHEN (count(*)%2=0) THEN count(*)/2+1 ELSE COUNT(*)/2 END as half FROM ranking
     ) cnt ON rk.rank = cnt.half
     JOIN teams t ON rk."teamID" = t."teamID"
 WHERE t."yearID" >= 1970
