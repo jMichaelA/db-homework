323abdfd4b121488cf5200c69cdc22b13f6986a1
kodekill <brian.falor@gmail.com>
2017-10-23 22:28:25 -0600

    Add files via upload

---
+++ query-5.sql
@@ -0,0 +1,16 @@
+WITH "Average" AS (
+        SELECT "yearID", b."masterID", 
+        CASE WHEN (b."AB" >= 1) AND (b."H" = 0) THEN ROUND (('0')::DECIMAL, 4)
+             WHEN (b."AB" > 1) AND (b."H" > 0) THEN ROUND ((b."H")  / (b."AB")::DECIMAL, 4)   END AS "aver" 
+        FROM batting b 
+        )
+
+SELECT DISTINCT a."aver" "Average", "H" "Hits", "AB" "At Bats","nameFirst" "First Name", "nameLast" "Last Name", b."yearID" "Year" 
+        FROM schools s
+        JOIN schoolsplayers sp ON sp."schoolID" = s."schoolID"
+        JOIN master m ON m."masterID" = sp."masterID"
+        JOIN batting b ON b."masterID" = sp."masterID"
+        JOIN "Average" a ON a."masterID" = b."masterID"
+        JOIN "Average" ON a."yearID" =  b."yearID"
+WHERE s."schoolName" = 'Utah State University' AND "AB" >= 0 
+ORDER BY b."yearID","nameLast", a."aver", "H", "AB","nameFirst"
